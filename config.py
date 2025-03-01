a6da66b9895b47468bca5b5a7594115ffa4e6a45
ambufenn <108204426+ambufenn@users.noreply.github.com>
2025-03-01 13:02:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 def set_environment_variables():
     # Set the environment variable
-    os.environ["GROQ_API_KEY"] = "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"
+    os.environ["GROQ_API_KEY"] = "gsk_AzzWupgdZh8N0Au01rh2WGdyb3FY6FHkdqaJoEaX4LG7T27J4xv"
